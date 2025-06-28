79051b988c1150d66fb914bc8fe29aa3e8f0a337
Ahsan Ali <ahsan.cooperativec@gmail.com>
2025-06-28 20:29:07 +0500

    version change

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,10 @@
+import axios from 'axios';
 type HttpMethod = 'get' | 'post' | 'put' | 'delete';
 interface UseApiOptions {
     manual?: boolean;
     params?: Record<string, any>;
 }
-declare function useApi<T = any>(endpoint: string, method?: HttpMethod, options?: UseApiOptions): {
+declare function useApi<T = any>(endpoint: string, method: HttpMethod | undefined, options: UseApiOptions | undefined, axiosInstance: typeof axios): {
     data: T | null;
     loading: boolean;
     error: string | null;
--- dist/index.js
+++ dist/index.js
@@ -8,30 +8,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = require("react");
 const react_native_1 = require("react-native");
-const axios_1 = __importDefault(require("axios"));
-//// you can create your custom axios instance in separate file and use it here. It is just for demonstration 
-const axiosInstance = axios_1.default.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-});
-//use this for authentication with token
-axiosInstance.interceptors.request.use((config) => {
-    const token = ''; // Add your token logic here
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => Promise.reject(error));
-function useApi(endpoint, method = 'get', options = {}) {
+function useApi(endpoint, method = 'get', options = {}, axiosInstance) {
     const [data, setData] = (0, react_1.useState)(null);
     const [loading, setLoading] = (0, react_1.useState)(!options.manual);
     const [error, setError] = (0, react_1.useState)(null);
@@ -78,26 +58,3 @@ const styles = react_native_1.StyleSheet.create({
     },
 });
 exports.default = useApi;
-// use it like this
-// interface Todo {
-//   id: number;
-//   title: string;
-//   completed: boolean;
-// }
-// const MyComponent = () => {
-//     const { data, error, LoaderComponent } = useApi<Todo[]>(
-//         '/posts',
-//         'get'
-//     );
-//     useEffect(() => {
-//         RNBootSplash.hide();
-//     }, [])
-//     console.log(error)
-//     return (
-//         <View>
-//             {LoaderComponent}
-//     
-//         </View>
-//     );
-// };
-// export default MyComponent;
