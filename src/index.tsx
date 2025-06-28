341bca1849d17fc81b05c11ad757508833e60e2e
Ahsan Ali <ahsan.cooperativec@gmail.com>
2025-06-28 20:27:57 +0500

    axios instance change

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,6 @@ import { ActivityIndicator, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 //// you can create your custom axios instance in separate file and use it here. It is just for demonstration 
-const axiosInstance = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-});
-
-//use this for authentication with token
-axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = ''; // Add your token logic here
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
 
 type HttpMethod = 'get' | 'post' | 'put' | 'delete';
 
@@ -33,7 +14,8 @@ interface UseApiOptions {
 function useApi<T = any>(
     endpoint: string,
     method: HttpMethod = 'get',
-    options: UseApiOptions = {}
+    options: UseApiOptions = {},
+    axiosInstance: typeof axios
 ) {
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(!options.manual);
@@ -92,35 +74,3 @@ const styles = StyleSheet.create({
 });
 
 export default useApi;
-
-
-
-// use it like this
-
-// interface Todo {
-//   id: number;
-//   title: string;
-//   completed: boolean;
-// }
-
-
-// const MyComponent = () => {
-//     const { data, error, LoaderComponent } = useApi<Todo[]>(
-//         '/posts',
-//         'get'
-//     );
-
-//     useEffect(() => {
-//         RNBootSplash.hide();
-//     }, [])
-
-//     console.log(error)
-//     return (
-//         <View>
-//             {LoaderComponent}
-//     
-//         </View>
-//     );
-// };
-
-// export default MyComponent;
